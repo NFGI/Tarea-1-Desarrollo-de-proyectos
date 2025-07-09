3e50a2ce4677f2d73f555ccba95e507472fd1057
Nicolas Gonzalez <Notiene@gmail.com>
2025-07-08 21:59:25 -0400

    cambio de caperta notebook de lectura de dataset

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,10 +1,5 @@
-# src/data/dataset.py
-
 import pandas as pd
 
 def cargar_parquet(url):
-    """
-    Carga un archivo Parquet desde una URL o ruta local.
-    """
     df = pd.read_parquet(url)
     return df
